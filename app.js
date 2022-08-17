5bf46d62d96717053cbc017bbdc80f1289f6a0ad
AlexMagar <lx.mgr07@gmail.com>
2022-08-17 21:42:35 +1000

    added body parser

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ const app = express();
 
 //to load static files
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // "/"=> home route
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
+});
+
+//post request, get the text from input filed 
+app.post("/", function(req, res){
+    var firstname = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+    console.log()
 });
 
 //setup server to listen in certain port
